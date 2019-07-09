5b2de372685e03aecc6c3264ca76f0baae1544b5
North <YangShi2@sf-express.com>
2019-07-09 15:45:43 +0800

    add gitignore

--- src/utils/av.js
+++
@@ -1,9 +0,0 @@
-import AV from 'leancloud-storage';
-
-AV.init("jSEFKegpVHveHRdim2kASMzV-gzGzoHsz", "sgbaSoyNpSod5Eu9N68FpcJk");
-// 调试信息
-if (process.env.NODE_ENV === 'development') {
-  localStorage.setItem('debug', 'leancloud*,LC*');
-}
-
-export default AV;
